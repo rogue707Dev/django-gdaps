0200aa499024e9cdceea667cf9c7a199379b6eb5
rogue707dev <rogue901126@protonmail.com>
2018-12-01 18:42:04 +0100

    add extensionpoint length tests

--- tests/test_extensionpoints.py
+++ tests/test_extensionpoints.py
@@ -1,9 +1,35 @@
 import pytest
 
-from gdaps import ExtensionPoint, Interface, PluginError
+from gdaps import ExtensionPoint, Interface, PluginError, implements
 from .interfaces import ITestInterface1, ITestInterface2
 from .test_interfaces import TestPlugin1, TestPlugin3
 
+
+# Test classes for interfaces and their implementations
+class ITestInterface3(Interface):
+    pass
+
+
+class ITestInterface4(Interface):
+    pass
+
+
+@implements(ITestInterface3)
+class Foo:
+    pass
+
+
+@implements(ITestInterface4)
+class Bar:
+    pass
+
+
+@implements(ITestInterface4)
+class Baz:
+    pass
+
+class ITestInterface5(Interface):
+    pass
 
 
 def test_empty_ep():
@@ -43,4 +69,15 @@ def test_direct_interface_extension():
 def test_ep_repr():
     ep = ExtensionPoint(ITestInterface2)
     assert ep.__repr__() == "<ExtensionPoint 'ITestInterface2'>"
+
+
+def test_ep_len():
+    ep = ExtensionPoint(ITestInterface5)
+    assert len(ep) == 0
+
+    ep = ExtensionPoint(ITestInterface3)
+    assert len(ep) == 1
+
+    ep = ExtensionPoint(ITestInterface4)
+    assert len(ep) == 2
 
