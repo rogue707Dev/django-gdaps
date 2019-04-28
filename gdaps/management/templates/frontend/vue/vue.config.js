0893966d651f14ac5a2380c41c5bda97c44dee01
rogue707dev <rogue901126@protonmail.com>
2019-04-28 03:09:37 +0200

    use localhost, not 0.0.0.0 in config (for development ATM)

--- gdaps/management/templates/frontend/vue/vue.config.js
+++ gdaps/management/templates/frontend/vue/vue.config.js
@@ -1,7 +1,7 @@
 const BundleTracker = require("webpack-bundle-tracker");
 
 module.exports = {
-    publicPath: 'http://0.0.0.0:8080',
+    publicPath: 'http://localhost:8080',
     outputDir: './dist/',
 
     chainWebpack: config => {
