0ee46d3f340c94f217726115ccf7539d30f4f99b
rogue707dev <rogue901126@protonmail.com>
2019-04-28 03:09:11 +0200

    mention GDAPS in frontend demo

--- gdaps/management/templates/frontend/vue/src/components/HelloWorld.vue
+++ gdaps/management/templates/frontend/vue/src/components/HelloWorld.vue
@@ -6,10 +6,8 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Installed GDAPS Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
