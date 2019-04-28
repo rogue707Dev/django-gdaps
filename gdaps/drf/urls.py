01dad518ebf8c55da09da40f4cba5657d89e598e
rogue707dev <rogue901126@protonmail.com>
2019-04-28 03:14:00 +0200

    introduce gdaps.drf.urls - dysfunct yet

---
+++ gdaps/drf/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+
+# automatically include Django-REST-Framework's URLs
+urlpatterns = []
+# FIXME: rest_framework cannod be loaded at this time...?
+# urlpatterns = [path("api/", include("rest_framework.urls", namespace="api"))]
