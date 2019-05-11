f75ddf168e9b333c1c44f175a46ffd398b051a0b
rogue707dev <rogue901126@protonmail.com>
2019-05-11 15:34:14 +0200

    frontend: make sure webpack_loader is in INSTALLED_APPS

--- gdaps/frontend/apps.py
+++ gdaps/frontend/apps.py
@@ -1,7 +1,9 @@
 import os
 
-from django.apps import AppConfig
+from django.apps import AppConfig, apps
 from django.conf import settings
+
+from gdaps import PluginError
 
 
 class FrontendConfig(AppConfig):
@@ -10,6 +12,11 @@ class FrontendConfig(AppConfig):
     verbose_name = "GDAPS frontend"
 
     def ready(self):
+
+        if "webpack_loader" not in [app.name for app in apps.get_app_configs()]:
+            raise PluginError(
+                "The 'gdaps.frontend' module relies on django-webpack-loader. Please add 'webpack_loader' to your INSTALLED_APPS."
+            )
 
         settings.WEBPACK_LOADER.update(
             {
