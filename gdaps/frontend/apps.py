676af67b930cb4db165e7f25ce67145d9b7c732f
rogue707dev <rogue901126@protonmail.com>
2019-05-11 14:27:06 +0200

    provide WEBPACK GDAPS configuration automatically

--- gdaps/frontend/apps.py
+++ gdaps/frontend/apps.py
@@ -1,7 +1,25 @@
+import os
+
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class FrontendConfig(AppConfig):
     name = "gdaps.frontend"
     label = "frontend"
     verbose_name = "GDAPS frontend"
+
+    def ready(self):
+
+        settings.WEBPACK_LOADER.update(
+            {
+                "GDAPS": {
+                    "STATS_FILE": os.path.join(
+                        # FIXME: use dynamic frontend dir
+                        settings.BASE_DIR,
+                        "frontend",
+                        "webpack-stats.json",
+                    )
+                }
+            }
+        )
