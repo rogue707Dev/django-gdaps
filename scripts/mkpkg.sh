c22684c4cfc8a0580637e9b5f60e265493313b0d
rogue707dev <rogue901126@protonmail.com>
2019-06-15 17:56:37 +0200

    enhance package-upload script

--- scripts/mkpkg.sh
+++ scripts/mkpkg.sh
@@ -6,13 +6,20 @@ die() {
     exit 1
 }
 
+if [ -d "dist/" ]; then
+    die "Please delete the dist/ directory before proceeding."
+fi
+
+version=$(grep "version=" setup.py| cut -d '"' -f2)
+[ "$version" == "" ] && die "Could not determine current version. Please check setup.py file."
 
 
 python -m keyring >/dev/null || die "Please install keyring in this virtualenv."
 
 python setup.py sdist bdist_wheel >/dev/null
 
-if [ "$1" != "-p" ]; then
+
+if [[ "$1" != "-p" ]]; then
     testing=1
     repo="https://test.pypi.org/legacy/"
     echo "Using test PyPi environment. Use 'mkpkg.sh -p' to upload to productive PyPi server."
@@ -23,8 +30,8 @@ fi
 
 python -m twine upload --repository-url ${repo} dist/* || die "Not able to upload tp ${repo}"
 
-if [ $testing ]; then
-    echo "Now test install your package using this code:"
+if [[ $testing ]]; then
+    echo "Now test-install your package using this code:"
     echo
     echo "python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps gdaps"
 fi
