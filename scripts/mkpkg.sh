f3573696aec7269f11fa51af27211ef2046cb7e0
rogue707dev <rogue901126@protonmail.com>
2019-06-15 17:19:23 +0200

    more automatisation of package creation/upload

--- scripts/mkpkg.sh
+++ scripts/mkpkg.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-python setup.py sdist bdist_wheel && python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+python setup.py sdist bdist_wheel
+
+if [ "$1" != "-p" ]; then
+    repo="https://test.pypi.org/legacy/"
+    echo "Uploading to test PyPi server repository. Use '-p' switch to upload to productive PyPi server"
+else
+    repo="https://pypi.org/legacy/"
+    echo "uploading productive PyPi server"
+fi
+
+python -m twine upload --repository-url ${repo} dist/*
+
+echo "Now test install your package using this code:"
+echo
+echo "python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps gdaps"
+
+
