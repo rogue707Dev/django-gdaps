2c8bad281d357d3a4e66596a5ecca6abf645488b
rogue707dev <rogue901126@protonmail.com>
2019-06-15 18:01:35 +0200

    fix RTD style error

--- docs/conf.py
+++ docs/conf.py
@@ -48,14 +48,12 @@ html_theme = "sphinx_rtd_theme"
 
 html_theme_options = {
     "canonical_url": "https://gdaps.readthedocs.io/",
-    "logo_only": False,
+    # "logo_only": False,
     "display_version": True,
     "prev_next_buttons_location": "both",
-    "style_external_links": True,
-    "style_nav_header_background": "white",
     # Toc options
-    "collapse_navigation": True,
-    "sticky_navigation": True,
+    # "collapse_navigation": True,
+    # "sticky_navigation": True,
     # "navigation_depth": 4,
     # "includehidden": True,
     # "titles_only": False,
