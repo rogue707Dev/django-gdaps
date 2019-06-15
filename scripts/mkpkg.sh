1eeff0a1a2023b3c8bb43f83567fb6e3b69fa3c3
rogue707dev <rogue901126@protonmail.com>
2019-06-15 17:46:54 +0200

    enhance upload script

--- scripts/mkpkg.sh
+++ scripts/mkpkg.sh
@@ -1,19 +1,31 @@
 #!/usr/bin/env bash
 
-python setup.py sdist bdist_wheel
+die() {
+    # die  with amessage
+    echo "ERROR:  $1"
+    exit 1
+}
+
+
+
+python -m keyring >/dev/null || die "Please install keyring in this virtualenv."
+
+python setup.py sdist bdist_wheel >/dev/null
 
 if [ "$1" != "-p" ]; then
+    testing=1
     repo="https://test.pypi.org/legacy/"
-    echo "Uploading to test PyPi server repository. Use '-p' switch to upload to productive PyPi server"
+    echo "Using test PyPi environment. Use 'mkpkg.sh -p' to upload to productive PyPi server."
 else
-    repo="https://pypi.org/legacy/"
-    echo "uploading productive PyPi server"
+    repo="https://upload.pypi.org/legacy/"
+    echo "Uploading productive PyPi server"
 fi
 
-python -m twine upload --repository-url ${repo} dist/*
+python -m twine upload --repository-url ${repo} dist/* || die "Not able to upload tp ${repo}"
 
-echo "Now test install your package using this code:"
-echo
-echo "python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps gdaps"
-
+if [ $testing ]; then
+    echo "Now test install your package using this code:"
+    echo
+    echo "python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps gdaps"
+fi
 
