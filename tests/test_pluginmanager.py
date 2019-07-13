b129c7f4223d74f8a2fd801aec782d4498763a29
rogue707dev <rogue901126@protonmail.com>
2019-07-13 12:35:31 +0200

    +comment

--- tests/test_pluginmanager.py
+++ tests/test_pluginmanager.py
@@ -2,4 +2,5 @@ from gdaps.pluginmanager import PluginManager
 
 
 def test_pluginmanager_findplugins_empty():
+    # try to find plugins from a nonexisting entry point
     assert PluginManager.find_plugins("gdapstest_foo786578645786.plugins") == []
