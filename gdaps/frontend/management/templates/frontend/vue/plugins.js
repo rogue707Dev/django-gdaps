a89f2ba1e28d53bd85177bcb74db235ea7b591d9
rogue707dev <rogue901126@protonmail.com>
2019-09-13 16:11:16 +0200

    add some hints to plugins.js

--- gdaps/frontend/management/templates/frontend/vue/plugins.js
+++ gdaps/frontend/management/templates/frontend/vue/plugins.js
@@ -1,1 +1,8 @@
+// plugins.js
+//
+// This is a special file that is created by GDAPS automatically
+// using the 'startplugin' management command.
+// Only touch this file if you exactly know what you are doing.
+// it will be overwritten with every run of 'manage.py startplugin <xyz>'
+
 module.exports = []
