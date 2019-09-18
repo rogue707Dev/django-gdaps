efd787674d120a6448c32f7ec3c0075eeb0a14ba
rogue707dev <rogue901126@protonmail.com>
2019-09-18 10:18:06 +0200

    remove unneeded app imports to speedup tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -4,10 +4,6 @@ SECRET_KEY = "test"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
 INSTALLED_APPS = [
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.admin",
     "gdaps",
     "tests.plugins.plugin1.apps.Plugin1Config",
 ]
