f84b731f8ada5cfb318bc55903fee214a7df7bfd
rogue707dev <rogue901126@protonmail.com>
2019-09-28 14:43:06 +0200

    add FRONTEND_ENGINE to defaults

--- gdaps/frontend/conf.py
+++ gdaps/frontend/conf.py
@@ -4,6 +4,9 @@ from gdaps.conf import PluginSettings
 
 NAMESPACE = "GDAPS"
 
-DEFAULTS = {"FRONTEND_DIR": os.path.join(settings.BASE_DIR, "frontend")}
+DEFAULTS = {
+    "FRONTEND_DIR": os.path.join(settings.BASE_DIR, "frontend"),
+    "FRONTEND_ENGINE": None,
+}
 
 frontend_settings = PluginSettings(NAMESPACE, None, DEFAULTS, None)
