5d492acc5fd5d7950aef1ecc04f5c66e1edc59c1
rogue707dev <rogue901126@protonmail.com>
2019-09-28 14:42:49 +0200

    let vue Engine use new API

--- gdaps/frontend/engines/vue.py
+++ gdaps/frontend/engines/vue.py
@@ -1,8 +1,13 @@
+import os
 import shutil
 import subprocess
+from typing import List
 
-from gdaps import implements
+from django.conf import settings
+
+from gdaps import implements, PluginError
 from gdaps.frontend.api import IFrontendEngine
+from gdaps.frontend import frontend_settings
 
 
 @implements(IFrontendEngine)
@@ -20,8 +25,8 @@ class VueEngine:
         "src/components/HelloWorld.vue",
     ]
 
-    def initialize(self, frontend_path):
-
+    @staticmethod
+    def initialize(frontend_path):
         try:
             # yarn install vue
             # FIXME: check if yarn is available
@@ -31,3 +36,41 @@ class VueEngine:
         except Exception as e:
             shutil.rmtree(frontend_path)
             raise e
+
+    @staticmethod
+    def update_plugins_list(plugins_list: List[str]) -> None:
+        """Writes plugins into plugins.js file, to be collected dynamically by webpack."""
+
+        global_frontend_path = os.path.join(
+            settings.BASE_DIR, frontend_settings.FRONTEND_DIR
+        )
+        if not os.path.exists(global_frontend_path):
+            try:
+                os.makedirs(global_frontend_path)
+            except:
+                raise PluginError(
+                    f"Could not create frontend directory '{global_frontend_path}'."
+                )
+        try:
+            plugins_file = open(os.path.join(global_frontend_path, "plugins.js"), "w")
+
+            plugins_file.write("module.exports = [\n")
+
+            counter = 1
+            total = len(plugins_list)
+            for app_name in plugins_list:
+                plugins_file.write('  "' + os.path.join(app_name, "frontend") + '"')
+                if counter < total:
+                    plugins_file.write(",")
+                plugins_file.write("\n")
+                counter += 1
+
+            plugins_file.write("]\n")
+        except Exception as e:
+            raise PluginError(
+                str(e)
+                + "\n "
+                + f"Could not open plugins.js for writing. Please check write permissions in {global_frontend_path}."
+            )
+        finally:
+            plugins_file.close()
