c36a98983625719669c34d61808e2917a82343ac
rogue707dev <rogue901126@protonmail.com>
2019-10-01 23:42:10 +0200

    init npm package (with package.json file) at plugin creation

--- gdaps/frontend/management/commands/startplugin.py
+++ gdaps/frontend/management/commands/startplugin.py
@@ -1,12 +1,16 @@
 import logging
 import os
+import shutil
+import subprocess
 import sys
 
 from django.apps import apps
+from django.core.management import CommandError
 
 from gdaps.frontend import current_engine
 from gdaps.pluginmanager import PluginManager
 from gdaps.management.commands.startplugin import Command as GdapsStartPluginCommand
+from gdaps.conf import gdaps_settings
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +25,9 @@ class Command(GdapsStartPluginCommand):
     )
 
     def handle(self, name, **options):
+
+        if shutil.which("npm") is None:
+            raise CommandError("npm is not available, please install it.")
 
         self.templates.append(
             os.path.join(
@@ -41,5 +48,11 @@ class Command(GdapsStartPluginCommand):
             logger.info("Found plugins:\n")
             for plugin in all_plugin_names:
                 logger.info("  " + plugin + "\n")
+
+        subprocess.check_call(
+            "npm init",
+            cwd=os.path.join(GdapsStartPluginCommand.plugin_path, name, gdaps_settings.FRONTEND_DIR),
+            shell=True,
+        )
 
         current_engine().update_plugins_list(all_plugin_names)
