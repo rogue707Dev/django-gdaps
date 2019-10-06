f878495b2437a92b12c819a7d930bdc75d254931
rogue707dev <rogue901126@protonmail.com>
2019-10-06 22:22:49 +0200

    correct relative frontend path setting

--- gdaps/frontend/conf.py
+++ gdaps/frontend/conf.py
@@ -5,7 +5,7 @@ from gdaps.conf import PluginSettings
 NAMESPACE = "GDAPS"
 
 DEFAULTS = {
-    "FRONTEND_DIR": os.path.join(settings.BASE_DIR, "frontend"),
+    "FRONTEND_DIR": "frontend",
     "FRONTEND_ENGINE": None,
     "STATS_FILE": os.path.join(settings.BASE_DIR, "frontend", "webpack-stats.json"),
 }
