8ccb85ada6599efe52b36e0eece807e39d0b6d90
rogue707dev <rogue901126@protonmail.com>
2019-10-20 20:20:24 +0200

    switch class order to remove circle import

--- gdaps/frontend/api.py
+++ gdaps/frontend/api.py
@@ -6,45 +6,6 @@ from gdaps import Interface
 
 
 @Interface
-class IFrontendEngine:
-    # FrontendEngines should not be instantiated.
-    __service__ = False
-
-    """The name of the frontend which can be used by GDAPS, e.g. 'vue', 'react', etc."""
-    name = None
-
-    """A list of tuples for template file endings that should be renamed and rendered as templates."""
-    rewrite_template_suffixes = ((".js-tpl", ".js"),)
-
-    """A list of (relative) file names that also be treated as templates."""
-    extra_files = []
-
-    extensions = ()
-
-    __package_manager = None
-
-    @classmethod
-    def initialize(cls, frontend_dir: str, package_manager: dict):
-        """Initializes engine.
-
-        This method is called when the frontend is created, and will be only called once.
-        It should install all frontend specific stuff, e.g. using Js libraries using 'yarn/npm install' etc.
-        It can assume that the BASE_DIR/frontend_dir/ exists.
-        :param frontend_dir: relative directory within BASE_DIR where the frontend lives.
-
-        """
-
-    @staticmethod
-    def update_plugins_list() -> None:
-        """Updates a list of plugins that the frontend can include dynamically then.
-
-        This can be different from frontend to frontend. Easiest way is to create a Javascript
-        module that exports an [array] of paths that point to modules that e.g. webpack then imports.
-        :param plugin_paths: a list of module names that contain a frontend directory with a Javascript module.
-        """
-
-
-@Interface
 class IPackageManager:
     """Interface for package manager representation.
 
@@ -71,3 +32,42 @@ class IPackageManager:
 
     def uninstall(self, pkg, cwd):
         raise NotImplementedError
+
+
+@Interface
+class IFrontendEngine:
+    # FrontendEngines should not be instantiated.
+    __service__ = False
+
+    """The name of the frontend which can be used by GDAPS, e.g. 'vue', 'react', etc."""
+    name = None
+
+    """A list of tuples for template file endings that should be renamed and rendered as templates."""
+    rewrite_template_suffixes = ((".js-tpl", ".js"),)
+
+    """A list of (relative) file names that also be treated as templates."""
+    extra_files = []
+
+    extensions = ()
+
+    __package_manager = None
+
+    @classmethod
+    def initialize(cls, frontend_dir: str, package_manager: IPackageManager):
+        """Initializes engine.
+
+        This method is called when the frontend is created, and will be only called once.
+        It should install all frontend specific stuff, e.g. using Js libraries using 'yarn/npm install' etc.
+        It can assume that the BASE_DIR/frontend_dir/ exists.
+        :param frontend_dir: relative directory within BASE_DIR where the frontend lives.
+        :param package_manager: the package manager to be used.
+        """
+
+    @staticmethod
+    def update_plugins_list() -> None:
+        """Updates a list of plugins that the frontend can include dynamically then.
+
+        This can be different from frontend to frontend. Easiest way is to create a Javascript
+        module that exports an [array] of paths that point to modules that e.g. webpack then imports.
+        :param plugin_paths: a list of module names that contain a frontend directory with a Javascript module.
+        """
